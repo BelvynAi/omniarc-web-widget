e5495da15862757e1581569050eded2ee8757954
BelvynAi <tbpvision101@gmail.com>
2025-11-14 22:11:35 +0100

    Update launcher-button.tsx

--- OMNIARC_WEB_WIDGET/components/launcher-button.tsx
+++ OMNIARC_WEB_WIDGET/components/launcher-button.tsx
@@ -5,9 +5,10 @@ interface LauncherButtonProps {
   hasUnread: boolean
   primaryColor: string
   accentColor: string
+  logoUrl?: string // Added optional logoUrl prop
 }
 
-export function LauncherButton({ onClick, hasUnread, primaryColor, accentColor }: LauncherButtonProps) {
+export function LauncherButton({ onClick, hasUnread, primaryColor, accentColor, logoUrl }: LauncherButtonProps) {
   return (
     <button
       onClick={onClick}
@@ -25,6 +26,9 @@ export function LauncherButton({ onClick, hasUnread, primaryColor, accentColor }
         justifyContent: "center",
         transition: "all 150ms ease",
         outline: "none",
+        backgroundImage: logoUrl ? `url(${logoUrl})` : "none",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
       onMouseEnter={(e) => {
         e.currentTarget.style.transform = "scale(1.05)"
@@ -39,9 +43,11 @@ export function LauncherButton({ onClick, hasUnread, primaryColor, accentColor }
         e.currentTarget.style.boxShadow = "0 4px 12px rgba(0, 0, 0, 0.15)"
       }}
     >
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2Z" fill="white" />
-      </svg>
+      {!logoUrl && (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2Z" fill="white" />
+        </svg>
+      )}
     </button>
   )
 }
